c413b009cd5995bb82f4d1c7513cceeb87135063
aayush1997-hub <63947837+aayush1997-hub@users.noreply.github.com>
2020-04-23 17:45:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./styles.css";
 import StopWatch from "./StopWatch.js";
 import ParagraphCounter from "./ParagraphCounter.js"
+import TransverseArray from "./TransverseArray.js"
 
 export default function App() {
   return (
     <div className="App">
       <StopWatch />
       <ParagraphCounter />
+    <TransverseArray />
     </div>
   );
 }
